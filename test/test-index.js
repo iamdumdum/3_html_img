9b615e57592af9fbb7ba7cc9fa03e35b3434df19
ozubergz <jmin499@gmail.com>
2020-11-02 13:40:39 -0500

    all test passes

--- test/test-index.js
+++ test/test-index.js
@@ -15,6 +15,19 @@ describe('index.html', () => {
         .then(done, done);
     });
 
-    
+    describe('Create a img tag', () => {
+        it('img tag should exist', () => {
+            const img = document.querySelector('img')
+            expect(img).to.exist
+        })
+        it('image tag should have a src attribute that points to the kitting image', () => {
+            const img = document.querySelector('img')
+            expect(img).to.have.attribute('src', 'https://bit.ly/fcc-relaxing-cat')           
+        })
+        it(`image element's alt attribute should not be empty`, () => {
+            const img = document.querySelector('img')
+            expect(img).to.have.attribute('alt').and.to.not.equal('')
+        })
+    })
 
 })
